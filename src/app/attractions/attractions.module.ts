098fe13e20eb1e7dbe97818706957d8413e9496f
johnj974 <jjdug@hotmail.com>
2021-05-20 08:45:32 +0100

    attraction page start/new images+component added

--- src/app/attractions/attractions.module.ts
+++ src/app/attractions/attractions.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AttractionsRoutingModule } from './attractions-routing.module';
 import { MainAttractionsComponent } from './main-attractions/main-attractions.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 @NgModule({
   declarations: [
-    MainAttractionsComponent
+    MainAttractionsComponent,
+    BannerComponent
   ],
   imports: [
     CommonModule,
