c05033c9dc22f67a759a032c8c73785753c84c49
johnj974 <jjdug@hotmail.com>
2021-05-22 13:37:16 +0100

    contact form added

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { MainContactComponent } from './main-contact/main-contact.component';
 import { LayoutModule } from '../layout/layout.module';
+import { FormComponent } from './form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [MainContactComponent],
-  imports: [CommonModule, ContactRoutingModule, LayoutModule],
+  declarations: [MainContactComponent, FormComponent],
+  imports: [
+    CommonModule,
+    ContactRoutingModule,
+    LayoutModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ContactModule {}
---
+++ src/app/contact/form/form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent implements OnInit {
+  //.
+  contactForm: FormGroup;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('[0-9]{10}'),
+      ]),
+      subject: new FormControl(null, Validators.required),
+      message: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.contactForm);
+  }
+}
